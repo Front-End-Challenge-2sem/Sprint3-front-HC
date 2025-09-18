49a2c7b007fbed846aa66d47d0d60d5f6d747fe6
yJoaoVictor10 <jvna07@gmail.com>
2025-09-18 20:57:39 -0300

    Colocando as rotas no main.tsx

--- sprint3-hc/src/main.tsx
+++ sprint3-hc/src/main.tsx
@@ -4,10 +4,17 @@ import App from './App.tsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Index from './routes/Index/index.tsx'
 import Error from './routes/Error/index.tsx'
+import Guia from './routes/Guia/index.tsx'
+import Integrantes from './routes/Integrantes/index.tsx'
+import FAQ from './routes/FAQ/index.tsx'
 
 const router = createBrowserRouter([
   {path: "/", element: <App/>, errorElement: <Error/>, children:[
     {path:"/", element:<Index/>},
+
+    {path:"/faq", element:<FAQ/>},
+    {path:"/integrantes", element:<Integrantes/>},
+    {path:"/guia", element:<Guia/>},
   ]}
 ])
 
