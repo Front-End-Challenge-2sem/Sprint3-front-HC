977bc80e797cf58d9c52d7879f61bc9add5e1d47
yJoaoVictor10 <jvna07@gmail.com>
2025-09-18 20:39:28 -0300

    Arrumando a extensão do tipoFaq e adicionando uma listaFaq para mostrar dados

--- sprint3-hc/src/routes/FAQ/index.tsx
+++ sprint3-hc/src/routes/FAQ/index.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { listaFaq } from "../../data/listaFaq";
 import type { TipoFaq } from "../../types/tipoFaq";
 
+
 export default function FAQ() {
   const [activeItem, setActiveItem] = useState<number | null>(null);
   const navigate = useNavigate();
