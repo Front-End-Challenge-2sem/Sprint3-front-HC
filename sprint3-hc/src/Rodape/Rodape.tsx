686c054934a45f4fa12978ec8dd97166eb3605ff
yJoaoVictor10 <jvna07@gmail.com>
2025-09-17 22:00:31 -0300

    Criação da parte visual do rodapé

--- sprint3-hc/src/Rodape/Rodape.tsx
+++ sprint3-hc/src/Rodape/Rodape.tsx
@@ -7,9 +7,66 @@ export default function Rodape() {
     const handleNavigation = (path: string) => {
       navigate(path);
     };
+    
     return(
-        <div>
-            
+        <footer className="rodape">
+        <div className="rodape-container">
+          <div className="rodape-secao">
+            <h3>Saúde Digital Acessível</h3>
+            <p>Facilitando o acesso à saúde digital para todos</p>
+          </div>
+          
+          <div className="rodape-secao">
+            <h3>Links Úteis</h3>
+            <ul className="rodape-links">
+              <li>
+                <a 
+                  href="/" 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigation('/');
+                  }}
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a 
+                  href="/guia" 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigation('/guia');
+                  }}
+                >
+                  Guia de Uso
+                </a>
+              </li>
+              <li>
+                <a 
+                  href="/faq" 
+                  onClick={(e) => {
+                    e.preventDefault();
+                    handleNavigation('/faq');
+                  }}
+                >
+                  Perguntas Frequentes
+                </a>
+              </li>
+            </ul>
+          </div>
+          
+          <div className="rodape-secao">
+            <h3>Contato</h3>
+            <address>
+              <p>Email: imrea@hc.fm.usp.br</p>
+              <p>Telefone: (11) 2661-8500</p>
+            </address>
+          </div>
         </div>
-    )
+        
+        <div className="rodape-bottom">
+          <p>&copy; 2025 Saúde Digital Acessível. Todos os direitos reservados.</p>
+        </div>
+      </footer>
+    );
 }
