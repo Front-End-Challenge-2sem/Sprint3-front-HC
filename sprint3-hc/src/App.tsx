737baeec2e81e33ff501a5e4404ce7c74a9a2871
yJoaoVictor10 <jvna07@gmail.com>
2025-09-17 23:04:17 -0300

    Criando a estrutura do App.tsx usando Outlet o react router dom

--- sprint3-hc/src/App.tsx
+++ sprint3-hc/src/App.tsx
@@ -1,7 +1,14 @@
+import { Outlet } from "react-router-dom";
+import Rodape from "./components/Rodape/Rodape";
+import Cabecalho from "./components/Cabecalho/Cabecalho";
+
 export default function App(){
   return(
     <div>
-      
+      <Cabecalho/>
+      <Outlet/>
+      <Rodape/>
     </div>
+
   )
 }
