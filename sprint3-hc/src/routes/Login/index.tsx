d1ea1ebd41d53bc0c390b44061f3145ebca9ece0
yJoaoVictor10 <jvna07@gmail.com>
2025-09-18 21:40:21 -0300

    Criando o botão de voltar usando navigate e props

--- sprint3-hc/src/routes/Login/index.tsx
+++ sprint3-hc/src/routes/Login/index.tsx
@@ -50,7 +50,17 @@ const formatCPF = (value: string) => {
   };
 
 
+  return(
+  <div className="pagina-login">
+        
+        <div className="voltar">
+          <button onClick={handleBackClick} className="botao-voltar">
+            <svg className="icone-voltar" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+            </svg>
+            Voltar
+          </button>
+        </div>
+        </div>
+  );
 }
-return(
-
-)
